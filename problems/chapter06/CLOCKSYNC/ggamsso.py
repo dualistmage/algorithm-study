5f76f01210974643d1d91adab6eaeface8647988
ggamsso <ggamsso@yahoo.co.kr>
2015-04-07 00:09:02 +0900

    updated

--- problems/chapter06/CLOCKSYNC/ggamsso.py
+++ problems/chapter06/CLOCKSYNC/ggamsso.py
@@ -1,65 +1,44 @@
 #!/usr/bin/env python
 
-switches = ( 
-    (0, 1, 2),
-    (0, 2, 14, 15),
-    (3, 7, 9, 11), 
-    (3, 14, 15),
-    (4, 10, 14, 15),
-    (4, 5, 7, 14, 15),
-    (0, 4, 5, 6, 7),
-    (1, 2, 3, 4, 5),
-    (6, 7, 8, 10, 12),
-    (3, 4, 5, 9, 13)
-)
+switches = [
+    (0, (0, 1, 2)),
+    (1, (0, 2, 14, 15)),
+    (2, (3, 7, 9, 11)), 
+    (3, (3, 14, 15)),
+    (4, (4, 10, 14, 15)),
+    (5, (4, 5, 7, 14, 15)),
+    (6, (0, 4, 5, 6, 7)),
+    (7, (1, 2, 3, 4, 5)),
+    (8, (6, 7, 8, 10, 12)),
+    (9, (3, 4, 5, 9, 13))
+]
 
-clockToSwitch = {}
-for switchNo in range(len(switches)):
-    for clockNo in switches[switchNo]:
-        if clockToSwitch.has_key(clockNo) == False:
-            clockToSwitch[clockNo] = set()
-        clockToSwitch[clockNo].add( switchNo )
+""" sort switch by maxClock """
+switches = [ (switchNo, clockNoList) for switchNo, clockNoList in sorted(switches, key=lambda switchInfo : len(switchInfo[1]), reverse=True) ]
 
-sample_count = int(raw_input())
+sampleMaxCount = int(raw_input())
 
-for sampleNo in range(sample_count):
+for sampleNo in range(sampleMaxCount):
     #inStr = "12 9 3 12 6 6 9 3 12 9 12 9 12 12 6 6" 
     inStr = raw_input()
-    inputArray = [ int(term)/4 for term in inStr.split() ]
+    clockStates = [ int(term)/4 for term in inStr.strip().split() ]
     switchList = []
-
-    switchToClock = {}
-    for clockNo in range(len(inputArray)):
-        for switchNo in clockToSwitch[clockNo]:
-            if switchToClock.has_key(switchNo) == False:
-                switchToClock[switchNo] = set()
-            switchToClock[switchNo].add(clockNo)
-
+    
     while True:
         """ check complete? """
-        if sum(inputArray) == 48:
+        if sum(clockStates) == (3*16):
             break
 
-        switchClockPosList = [ (switchNo, switchToClock[switchNo]) for switchNo in sorted( switchToClock, key=lambda x : len(switchToClock[x]), reverse=True ) ]
-        switchClockInfoList = [ (switchNo, [(clockNo, inputArray[clockNo]) for clockNo in clockPosList]) for switchNo, clockPosList in switchClockPosList ]
-
-        #print '#' * 80
-        #for swichNo, clockInfoList in switchClockInfoList:
-        #    print swichNo, clockInfoList
-
         """ clock eval """
-        for swichNo, clockInfoList in sorted( switchClockInfoList, key=lambda x : (sum([ clockInfo for clockNo, clockInfo in x[1]])/float(len(x[1]))) ):
-            """ clock!!! """
-            for clockNo, clockInfo in clockInfoList:
-                inputArray[clockNo] = clockInfo+1
-                if inputArray[clockNo] == 4:
-                    inputArray[clockNo] = 0
-            #print '*' * 80
-            #print inputArray, sum(inputArray), swichNo
+        for swichNo, clockNoList in sorted( switches, key=lambda switchInfo : (sum([ clockStates[clockNo] for clockNo in switchInfo[1]])/float(len(switchInfo[1]))) ):
+            """ switch click!!! """
+            for clockNo in clockNoList:
+                clockStates[clockNo] += 1
+                if clockStates[clockNo] == 4:
+                    clockStates[clockNo] = 0
+            #print '*' * 60
+            #print clockStates, swichNo, sum(clockStates)
             switchList.append( swichNo )
             break
 
-    #print '#' * 80
-    #print inputArray
-    #print switchList, len(switchList)
     print len(switchList)
