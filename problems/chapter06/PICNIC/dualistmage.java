9a25098aa13a7043ab49ff3c0c74c80ec38dc853
Dohyun Yun <finsternis@me.com>
2015-04-07 15:59:40 +0900

    Fixed PICNIC code to pass algospot

--- problems/chapter06/PICNIC/dualistmage.java
+++ problems/chapter06/PICNIC/dualistmage.java
@@ -1,38 +1,48 @@
-package chapter06.PICNIC;
+//package chapter06.PICNIC;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 class Reader {
-    static BufferedReader reader;
-    static StringTokenizer tokenizer;
 
-    /** call this method to initialize reader for InputStream */
+    // /** For BufferedReader **/
+    //
+    // static BufferedReader reader;
+    // static StringTokenizer tokenizer;
+    //
+    // static void init(InputStream input) {
+    //     reader = new BufferedReader(
+    //             new InputStreamReader(input) );
+    //     tokenizer = new StringTokenizer("");
+    // }
+    //
+    // static String next() throws IOException {
+    //     while ( ! tokenizer.hasMoreTokens() ) {
+    //         //TODO add check for eof if necessary
+    //         tokenizer = new StringTokenizer(
+    //                 reader.readLine() );
+    //     }
+    //     return tokenizer.nextToken();
+    // }
+    //
+    // static int nextInt() throws IOException {
+    //     return Integer.parseInt( next() );
+    // }
+    //
+    // static double nextDouble() throws IOException {
+    //     return Double.parseDouble( next() );
+    // }
+
+    /** For Scanner **/
+    static Scanner scan;
+
     static void init(InputStream input) {
-        reader = new BufferedReader(
-                new InputStreamReader(input) );
-        tokenizer = new StringTokenizer("");
-    }
-
-    /** get next word */
-    static String next() throws IOException {
-        while ( ! tokenizer.hasMoreTokens() ) {
-            //TODO add check for eof if necessary
-            tokenizer = new StringTokenizer(
-                    reader.readLine() );
-        }
-        return tokenizer.nextToken();
+        scan = new Scanner(input);
     }
 
     static int nextInt() throws IOException {
-        return Integer.parseInt( next() );
-    }
-
-    static double nextDouble() throws IOException {
-        return Double.parseDouble( next() );
+        return scan.nextInt();
     }
 }
 public class dualistmage {
